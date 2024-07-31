d27eb6207e978b320ddc5d91b762fe8022b1986d
Angel Pena <theangel3535@gmail.com>
2024-07-30 22:09:22 -0400

    add git pluggins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,14 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require('gitsigns').setup()
-  end
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+
+      --vim.keymap.set("n", "<leader>GL", ":Gitsigns preview_hunk_inline<CR>")
+      vim.keymap.set("n", "<leader>GP", ":Gitsigns preview_hunk<CR>", {})
+    end
+  }
 }
