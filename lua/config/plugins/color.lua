88e1ddbe056404f4852433972251c934e1c049a9
Angel Pena <theangel3535@gmail.com>
2025-10-19 16:30:00 -0400

    stuff

--- lua/config/plugins/color.lua
+++ lua/config/plugins/color.lua
@@ -1,13 +1,29 @@
 return {
-  "vague2k/vague.nvim",
+  "ellisonleao/gruvbox.nvim",
   config = function()
-    require("vague").setup({
-      transparent = true,
-      colors = {
-        bg = "#080808",
+    require("gruvbox").setup({
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
       },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = false,   -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
     })
-    vim.cmd.colorscheme("vague")
-    vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#333333" })
+    vim.cmd("colorscheme gruvbox")
   end
 }
